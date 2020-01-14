7daeb221b68acb05ade93ac66f0f20267bffead0
Darragh Ferry <darraghferry@hotmail.com>
2020-01-14 19:53:33 +0000

    created post endpoint at /api/posts

--- data/db-router.js
+++ data/db-router.js
@@ -26,4 +26,18 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  if (!req.body.contents || !req.body.title) {
+    res.status(404).json({ message: "please enter titile and contents" });
+  } else {
+    Posts.insert(req.body)
+      .then(posts => {
+        res.status(201).json(posts);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "something went wrong" });
+      });
+  }
+});
+
 module.exports = router;
